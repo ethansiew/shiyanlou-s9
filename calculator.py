9fff18366a73517499c4ffaba575fa9358f1c363
ethansiew <siewfonglam@gmail.com>
2018-12-12 03:33:06 +0800

    new

--- calculator.py
+++ calculator.py
@@ -3,3 +3,24 @@ import sys
 import csv
 from collections import namedtuple
 
+
+IncomeTaxQuickLookupItem = namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
+
+
+INCOME_TAX_START_POINT = 3500
+
+
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 13505),
+    IncomeTaxQuickLookupItem(55000, 0.35, 5505),
+    IncomeTaxQuickLookupItem(35000, 0.30, 2755),
+    IncomeTaxQuickLookupItem(9000, 0.25, 1005),
+    IncomeTaxQuickLookupItem(4500, 0.2, 555),
+    IncomeTaxQuickLookupItem(1500, 0.1, 105),
+    IncomeTaxQuickLookupItem(0, 0.03, 0)
+]
+
+
